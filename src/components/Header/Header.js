8ee70116b068d0c18abcfb2babc853e86f2f52d9
Anik-the-dev <niksctg00@gmail.com>
2022-04-04 10:59:12 +0600

    header part css done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,16 +7,19 @@ const Header = () => {
     return (
         <div className='project-body'>
             <div className='project-container'>
-                <div className='project-logo'>
-                    <h1>niksrchery.</h1>
+                <div className='flex'>
+                    <div className='project-logo'>
+                        <h1>niksrchery.</h1>
+                    </div>
+                    <nav className='navbar'>
+                        <Link to='/'>Home</Link>
+                        <Link to='/reviews'>Reviews</Link>
+                        <Link to='/dashboard'>Dashboard</Link>
+                        <Link to='/blog'>Blog</Link>
+                        <Link to='/about'>About</Link>
+                    </nav>
+
                 </div>
-                <nav className='navbar'>
-                    <Link to= '/'>Home</Link>
-                    <Link to= '/reviews'>Reviews</Link>
-                    <Link to= '/dashboard'>Dashboard</Link>
-                    <Link to= '/blog'>Blog</Link>
-                    <Link to= '/about'>About</Link>
-                </nav>
 
             </div>
 
