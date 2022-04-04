dd07697c84ae8d53f3a771fb280ec0efa6a2643c
Anik-the-dev <niksctg00@gmail.com>
2022-04-04 19:42:08 +0600

    rechart loaded Done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line } from 'recharts';
+
 
 const Dashboard = () => {
+    const [data , setData] = useState([])
+    useEffect(()=>{
+        fetch('sellData.json')
+        .then(r =>r.json())
+        .then(d=>setData(d))
+    },[])
+
+    const {investment,month,sell,revenue} = data
+
+    const renderLineChart = (
+        <LineChart width={400} height={400} data={data}>
+            <Line type="month" dataKey="sell" stroke="red" />
+        </LineChart>
+    );
     return (
-        <div>
-            
+        <div className='project-container'>
+            <h2>Aniks Sells Revenue</h2>
+            {renderLineChart}
         </div>
     );
 };
