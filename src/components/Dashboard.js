08cfbf22cc912599cc10b56d9345e209f317de26
Anik-the-dev <niksctg00@gmail.com>
2022-04-04 20:05:40 +0600

    lineChart Done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,26 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import { LineChart, Line } from 'recharts';
+import React, { PureComponent, useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 
 const Dashboard = () => {
-    const [data , setData] = useState([])
-    useEffect(()=>{
+    const [data, setData] = useState([])
+    useEffect(() => {
         fetch('sellData.json')
-        .then(r =>r.json())
-        .then(d=>setData(d))
-    },[])
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
 
-    const {investment,month,sell,revenue} = data
+    // destructuring Object Data.........
+    const { investment, month, sell, revenue } = data
+
+    // Charts.............
+    // 1. SimpleLineChart..........
 
     const renderLineChart = (
-        <LineChart width={400} height={400} data={data}>
-            <Line type="month" dataKey="sell" stroke="red" />
+        <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+        >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="investment" stroke="#82ca9d" />
         </LineChart>
     );
     return (
-        <div className='project-container'>
-            <h2>Aniks Sells Revenue</h2>
-            {renderLineChart}
+        <div className='project-body  project-container'>
+            <h1 className='text-center lg mb-50'>niksrchery Sells Revenue</h1>
+            <div className='grid two-columns-grid'>
+                <div>
+                    {renderLineChart}
+                </div>
+                <div>
+                    {renderLineChart}
+                </div>
+                <div>
+                    {renderLineChart}
+                </div>
+                <div>
+                    {renderLineChart}
+                </div>
+
+            </div>
         </div>
     );
 };
