6276e685f854adcbbd5eb8ed79c16f900549e839
Anik-the-dev <niksctg00@gmail.com>
2022-04-04 09:53:07 +0600

    Header logo done! project container style done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../css/style.css'
+
+const Header = () => {
+    return (
+        <div className='project-body'>
+            <div className='project-container'>
+                <div className='project-logo'>
+                    <h1>niksrchery.</h1>
+                </div>
+                <nav className='navbar'></nav>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Header
